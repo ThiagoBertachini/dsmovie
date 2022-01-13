44648e4f9bf37f0d62652a4061a4f088f42f7d02
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-01-13 08:39:57 -0300

    Domain model, database seed

--- backend/dsmovie/src/main/java/com/bertachini/dsmovie/entities/Score.java
+++ backend/dsmovie/src/main/java/com/bertachini/dsmovie/entities/Score.java
@@ -1,5 +1,46 @@
 package com.bertachini.dsmovie.entities;
 
-public class Score {
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	
+	/* ID COMPOSTO */
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	public Score() {
+	}
+
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	
 }
